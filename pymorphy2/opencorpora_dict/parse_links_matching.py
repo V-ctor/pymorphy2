e57da3c2afca9e8422b02750d1ac578670064b57
vevitchenko <viktor.evitchenko@sitesoft.ru>
2018-05-25 12:44:43 +0500

    Refactored links parser. (#PIM-462)

--- pymorphy2/opencorpora_dict/parse_links_matching.py
+++ pymorphy2/opencorpora_dict/parse_links_matching.py
@@ -41,20 +41,36 @@ def parse_links_matching_xml(filename):
 
     logger.info("Parsing file links matching dictionary v%s", filename)
 
-    page = etree.parse(filename)
-    root = page.getroot()
-    print(root)
-    if root.tag == "allowed_link_types":
-        print(root.getchildren())
-        for allowed_link_type in root.getchildren():
-            links = []
-            if allowed_link_type.tag == "link_type":
-                id = int(allowed_link_type.get('id'))
-                allowed_link_types[id] = links
-                for link in allowed_link_type.getchildren():
-                    id = int(link.get('id'))
-                    links.append(id)
-                    print("%s => %s" % (link.tag, link.text))
-                print()
-    print(allowed_link_types)
+    root = etree.parse(filename).getroot()
+    if does_element_contain_allowed_link_types_tag(root):
+        add_allowed_link_types(allowed_link_types, root)
     return allowed_link_types
+
+
+def add_allowed_link_types(allowed_link_types, element):
+    for allowed_link_type in element.getchildren():
+        links = []
+        if does_element_contain_link_types_tag(allowed_link_type):
+            id = get_id_from_element(allowed_link_type)
+            allowed_link_types[id] = links
+            add_links_from_linktype(allowed_link_type, links)
+
+
+def add_links_from_linktype(allowed_link_type, links):
+    for link in allowed_link_type.getchildren():
+        id = get_id_from_element(link)
+        links.append(id)
+        logger.info("%s => %s" % (link.tag, link.text))
+
+
+def get_id_from_element(link):
+    id = int(link.get('id'))
+    return id
+
+
+def does_element_contain_allowed_link_types_tag(element):
+    return element.tag == "allowed_link_types"
+
+
+def does_element_contain_link_types_tag(element):
+    return element.tag == "link_type"
