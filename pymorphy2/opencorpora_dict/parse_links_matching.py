6d33d170cf27f9605755db78ab3939d64620d630
vevitchenko <viktor.evitchenko@sitesoft.ru>
2018-05-25 18:59:34 +0500

    Refactored links parser and added tests. (#PIM-462)

--- pymorphy2/opencorpora_dict/parse_links_matching.py
+++ pymorphy2/opencorpora_dict/parse_links_matching.py
@@ -29,43 +29,49 @@ except ImportError:
     def xml_clear_elem(elem):
         elem.clear()
 
-
 logger = logging.getLogger(__name__)
 
 ParsedDictionary = collections.namedtuple('ParsedDictionary', 'lexemes links grammemes version revision')
 
 
 def parse_links_matching_xml(filename):
-
     allowed_link_types = {}
 
     logger.info("Parsing file links matching dictionary v%s", filename)
 
     root = etree.parse(filename).getroot()
     if does_element_contain_allowed_link_types_tag(root):
-        add_allowed_link_types(allowed_link_types, root)
+        add_allowed_link_types(root, allowed_link_types)
     return allowed_link_types
 
 
-def add_allowed_link_types(allowed_link_types, element):
+def add_allowed_link_types(element, allowed_link_types):
     for allowed_link_type in element.getchildren():
         links = []
         if does_element_contain_link_types_tag(allowed_link_type):
-            id = get_id_from_element(allowed_link_type)
+            id = get_id_from_link_type(allowed_link_type)
             allowed_link_types[id] = links
-            add_links_from_linktype(allowed_link_type, links)
+            add_links_from_link_type(allowed_link_type, links)
 
 
-def add_links_from_linktype(allowed_link_type, links):
-    for link in allowed_link_type.getchildren():
-        id = get_id_from_element(link)
-        links.append(id)
+def add_links_from_link_type(parent_element, link_list):
+    for link in parent_element.getchildren():
+        id = get_id_from_link(link)
+        if id is not None:
+            link_list.append(id)
         logger.info("%s => %s" % (link.tag, link.text))
 
 
-def get_id_from_element(link):
-    id = int(link.get('id'))
-    return id
+def get_id_from_link_type(element):
+    return None if element.tag != "link_type" else get_id_from_element(element)
+
+
+def get_id_from_link(element):
+    return None if element.tag != "link" else get_id_from_element(element)
+
+
+def get_id_from_element(element):
+    return None if element.get('id') is None else try_parse_int(element.get('id'))
 
 
 def does_element_contain_allowed_link_types_tag(element):
@@ -74,3 +80,10 @@ def does_element_contain_allowed_link_types_tag(element):
 
 def does_element_contain_link_types_tag(element):
     return element.tag == "link_type"
+
+
+def try_parse_int(s, base=10, val=None):
+    try:
+        return int(s, base)
+    except ValueError:
+        return val
--- tests/test_parse_links_matching.py
+++ tests/test_parse_links_matching.py
@@ -4,18 +4,104 @@ import os
 
 import pytest
 
-import pymorphy2
-from pymorphy2.opencorpora_dict.compile import (
-    _to_paradigm,
-    convert_to_pymorphy2
-)
-from pymorphy2.opencorpora_dict.parse import parse_opencorpora_xml
-from pymorphy2.dawg import assert_can_create
-from pymorphy2 import lang
-from pymorphy2.opencorpora_dict.parse_links_matching import parse_links_matching_xml
+from pymorphy2.opencorpora_dict.parse_links_matching import parse_links_matching_xml, get_id_from_element, \
+    does_element_contain_allowed_link_types_tag, add_links_from_link_type, add_allowed_link_types
+
+try:
+    from lxml import etree
+
+    print("running with lxml.etree")
+    from lxml.etree import iterparse
 
 
-class TestToyDictionary:
+    def xml_clear_elem(elem):
+        elem.clear()
+        while elem.getprevious() is not None:
+            del elem.getparent()[0]
+
+except ImportError:
+    try:
+        from xml.etree.cElementTree import iterparse
+    except ImportError:
+        from xml.etree.ElementTree import iterparse
+
+
+@pytest.mark.parametrize("tag, attr, attr_value, expected", [
+    ("link", "id", "1", 1),
+    ("links", "id", "1", 1),
+    ("link", "_id", "1", None),
+    ("link", "id", "0.5", None),
+    ("link", "id", "", None)
+])
+def test_id_parser(tag, attr, attr_value, expected):
+    element = etree.Element(tag)
+    element.set(attr, attr_value)
+    actual = get_id_from_element(element)
+    assert expected == actual
+
+
+@pytest.mark.parametrize("tag, expected", [
+    ("allowed_link_types", True),
+    ("allowed_link_type", False),
+    ("allowed_link", False)
+])
+def test_does_element_contain_allowed_link_types_tag_test(tag, expected):
+    element = etree.Element(tag)
+    actual = does_element_contain_allowed_link_types_tag(element)
+    assert expected == actual
+
+
+@pytest.mark.parametrize("xml, expected", [
+    ("<link_type id=\"1\"/>", []),
+    ("<link_type id=\"1\"></link_type>", []),
+    ("""<link_type id="21">
+            <link id="1"/>
+        </link_type>
+    """, [1]),
+    ("""<link_type id="21">
+            <link id="1"/>
+            <link id="2"/>
+        </link_type>
+    """, [1, 2])
+])
+def test_add_links_from_link_type(xml, expected):
+    element = etree.fromstring(xml)
+    links = []
+    add_links_from_link_type(element, links)
+    assert expected == links
+
+
+@pytest.mark.parametrize("xml, expected", [
+    ("""<allowed_link_types>
+        </allowed_link_types>
+    """, {}),
+    ("""<allowed_link_types>
+            <link_type id="1"/> <!--all connections with link type id 1 is allowed-->
+        </allowed_link_types>
+    """, {1: []}),
+    ("""<allowed_link_types>
+            <link_type id="1"/> <!--all connections with link type id 1 is allowed-->
+            <link_type id="2"/>
+        </allowed_link_types>
+    """, {1: [], 2: []}),
+    ("""<allowed_link_types>
+            <link_type id="1"/> <!--all connections with link type id 1 is allowed-->
+            <link_type id="2"/>
+            <link_type id="21">
+                <link id="100"/> <!--these links are allowed-->
+                <link id="200"/>
+            </link_type>
+        </allowed_link_types>
+    """, {1: [], 2: [], 21: [100, 200]})
+])
+def test_add_allowed_link_types(xml, expected):
+    element = etree.fromstring(xml)
+    links = {}
+    add_allowed_link_types(element, links)
+    assert expected == links
+
+
+class TestLinksMatchingParser:
     XML_PATH = os.path.join(
         os.path.dirname(__file__),
         '..',
